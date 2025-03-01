c295923f0c25067416146083c77714a1cbdb0fd1
Nour Eldien Ayman <nourayman189@gmail.com>
2025-03-01 16:32:36 +0200

    remove test variable.

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -18,7 +18,6 @@ public class ProductRepository extends MainRepository<Product> {
     @SuppressWarnings("checkstyle:VisibilityModifier")
     public static List<Product> products = new ArrayList<>();
     private static final double FULL_PERCENTAGE = 100.0;
-    public String id;
 
     public ProductRepository() {
 
