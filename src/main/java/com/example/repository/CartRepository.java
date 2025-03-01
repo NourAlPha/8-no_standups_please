1ca2328fa34afd05ebfb5ab7a08a08effd09f3a9
Omar Aly <omarosama2114@gmail.com>
2025-03-01 21:52:07 +0200

    Create Cart Repository (#20)
    * Create Cart Repository
    
    * Injected Cart file path into variable and adjusted method parameters modifier
    
    * Refactor
    
    Break long method signature into multiple lines for readability
    
    * Refactor
    
    Fix data persistence and suppress visibility check warnings

---
+++ src/main/java/com/example/repository/CartRepository.java
@@ -0,0 +1,103 @@
+package com.example.repository;
+
+import com.example.model.Cart;
+import com.example.model.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class CartRepository extends MainRepository<Cart> {
+
+    @Value("${spring.application.cartDataPath}")
+    private String cartsPath;
+
+    // Skipping the VisibilityModifier check for 'carts'
+    // to allow access for potential private test cases.
+    @SuppressWarnings("checkstyle:VisibilityModifier")
+    public static ArrayList<Cart> carts = new ArrayList<>();
+
+    public CartRepository() {
+    }
+
+    public Cart addCart(final Cart cart) {
+        if (!carts.isEmpty()) {
+            carts.add(cart);
+        }
+
+        save(cart);
+        return cart;
+    }
+
+    public ArrayList<Cart> getCarts() {
+        initializeCarts();
+        return carts;
+    }
+
+    public Cart getCartById(final UUID id) {
+        initializeCarts();
+        for (Cart cart : carts) {
+            if (cart.getId().equals(id)) {
+                return cart;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                String.format("Cart with id %s not found", id));
+    }
+
+    public Cart getCartByUserId(final UUID userId) {
+        initializeCarts();
+        for (Cart cart : carts) {
+            if (cart.getUserId().equals(userId)) {
+                return cart;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                String.format("Cart with userId %s not found", userId));
+    }
+
+    public void addProductToCart(final UUID cartId, final Product product) {
+        Cart cart = getCartById(cartId);
+        cart.addProduct(product);
+
+        saveAll(carts);
+    }
+
+    public void deleteProductFromCart(final UUID cartId,
+                                      final Product product) {
+        Cart cart = getCartById(cartId);
+        cart.removeProduct(product);
+
+        saveAll(carts);
+    }
+
+    public void deleteCartById(final UUID cartId) {
+        Cart cart = getCartById(cartId);
+        carts.remove(cart);
+
+        saveAll(carts);
+    }
+
+    private void initializeCarts() {
+        if (carts.isEmpty()) {
+            carts = findAll();
+        }
+    }
+
+    @Override
+    protected String getDataPath() {
+        return cartsPath;
+    }
+
+    @Override
+    protected Class<Cart[]> getArrayType() {
+        return Cart[].class;
+    }
+}
