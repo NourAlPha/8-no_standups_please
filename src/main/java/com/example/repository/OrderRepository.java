f4ce6d97188fb6d7902b58141fed3a3f98d797f4
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2025-03-02 15:35:13 +0200

    (orders) adding orders repository (#23)
    * feat(orders) : adding orders repository
    
    * refactor(orders) : checkstyle errors fix orders repository
    
    * refactor(orders) : checkstyle error fix orders repository

---
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -0,0 +1,65 @@
+package com.example.repository;
+
+import com.example.model.Order;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class OrderRepository extends MainRepository<Order> {
+    @Value("${spring.application.orderDataPath}")
+    private String ordersPath;
+
+
+    @SuppressWarnings("checkstyle:VisibilityModifier")
+    public static ArrayList<Order> orders = new ArrayList<>();
+
+
+    public void addOrder(final Order order) {
+        orders.add(order);
+        save(order);
+    }
+
+    public ArrayList<Order> getOrders() {
+        initializeOrders();
+        return orders;
+    }
+
+    public Order getOrderById(final UUID orderId) {
+        initializeOrders();
+        for (Order order : orders) {
+            if (order.getId().equals(orderId)) {
+                return order;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                String.format("Order with id %s not found", orderId));
+    }
+
+    public void deleteOrderById(final UUID orderId) {
+        Order order = getOrderById(orderId);
+        orders.remove(order);
+        overrideData(orders);
+    }
+
+    @Override
+    protected String getDataPath() {
+        return ordersPath;
+    }
+
+    @Override
+    protected Class<Order[]> getArrayType() {
+        return Order[].class;
+    }
+
+    private void initializeOrders() {
+        if (orders.isEmpty()) {
+            orders.addAll(findAll());
+        }
+    }
+}
