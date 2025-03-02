49cb2319eabbb6ef94731261fde47355d1d593b2
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2025-03-02 14:47:29 +0200

    Mohamed/user requirments (#22)
    * added user repository
    
    * added user repository
    
    * added user repository
    
    * changed parameters to be final
    
    * formatted file
    
    * user repository done
    
    * modified line 40 length
    
    * moved + to be in a new line
    
    * user repository done

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,95 @@
+package com.example.repository;
+
+import com.example.model.Order;
+import com.example.model.User;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@SuppressWarnings("rawtypes")
+public class UserRepository extends MainRepository<User> {
+
+    @Value("${spring.application.userDataPath}")
+    private String usersPath;
+
+    private static ArrayList<User> users;
+
+    public UserRepository() {
+
+    }
+
+    public ArrayList<User> getUsers() {
+        intializeUsers();
+        return users;
+    }
+
+    public User getUserById(final UUID userId) {
+        intializeUsers();
+        for (User user : users) {
+            if (user.getId().equals(userId)) {
+                return user;
+            }
+        }
+        throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "User not " + "found");
+    }
+
+    public User addUser(final User user) {
+        intializeUsers();
+        users.add(user);
+        save(user);
+        return user;
+    }
+
+    public List<Order> getOrdersByUserId(final UUID userId) {
+        User user = getUserById(userId);
+        return user.getOrders();
+    }
+
+    public void addOrderToUser(final UUID userId, final Order order) {
+        User user = getUserById(userId);
+        user.addOrder(order);
+        overrideData(users);
+    }
+
+    public void removeOrderFromUser(final UUID userId, final UUID orderId) {
+        User user = getUserById(userId);
+        List<Order> orders = user.getOrders();
+        for (Order order : orders) {
+            if (order.getId().equals(orderId)) {
+                user.removeOrder(order);
+                break;
+            }
+        }
+        overrideData(users);
+    }
+
+    public void deleteUserById(final UUID userId) {
+        User user = getUserById(userId);
+        users.remove(user);
+        overrideData(users);
+    }
+
+    @Override
+    protected String getDataPath() {
+        return usersPath;
+    }
+
+    @Override
+    protected Class<User[]> getArrayType() {
+        return User[].class;
+    }
+
+    private void intializeUsers() {
+        if (users == null) {
+            users = findAll();
+        }
+    }
+}
