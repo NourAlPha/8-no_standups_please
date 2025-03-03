b24134920e5c92636cfc485007a2fbae76eb6ba7
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2025-03-03 09:27:41 +0200

    Add GenericRepository.java to reduce code duplicates blocks. (#24)

---
+++ src/main/java/com/example/repository/GenericRepository.java
@@ -0,0 +1,52 @@
+package com.example.repository;
+
+import com.example.model.Identifiable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public abstract class GenericRepository<T extends Identifiable>
+                                                    extends MainRepository<T> {
+
+    private ArrayList<T> objects;
+
+    protected ArrayList<T> getObjects() {
+        initializeObjects();
+        return objects;
+    }
+
+    protected T addObject(final T object) {
+        if (objects != null) {
+            objects.add(object);
+        }
+
+        save(object);
+        return object;
+    }
+
+    protected T getObjectById(final UUID id) {
+        initializeObjects();
+        for (T object : objects) {
+            if (object.getId().equals(id)) {
+                return object;
+            }
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                            String.format("id %s not found", id));
+    }
+
+    protected void deleteObjectById(final UUID id) {
+        T object = getObjectById(id);
+        objects.remove(object);
+        overrideData(objects);
+    }
+
+    private void initializeObjects() {
+        if (objects == null) {
+            objects = findAll();
+        }
+    }
+}
