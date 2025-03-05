4f2f988e511a698961da11e39a075623945dfce9
Omar Aly <omarosama2114@gmail.com>
2025-03-05 16:55:57 +0200

    Remove unused imports

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.example.repository;
 
-import com.example.model.Cart;
 import com.example.model.Order;
 import com.example.model.Product;
 import org.springframework.beans.factory.annotation.Value;
