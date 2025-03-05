d0240ea00fd6f896cab582e79407837d2b59ab04
Nour Eldien Ayman <nourayman189@gmail.com>
2025-03-05 14:48:51 +0200

    Add OrderService.java which implements service logic of Order

---
+++ src/main/java/com/example/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class OrderService extends MainService<Order> {
+
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public OrderService(final OrderRepository orderRepository,
+                        final UserRepository userRepository) {
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void addOrder(final Order order) {
+        orderRepository.addOrder(order);
+    }
+
+    public ArrayList<Order> getOrders() {
+        return orderRepository.getOrders();
+    }
+
+    public Order getOrderById(final UUID orderId) {
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public void deleteOrderById(final UUID orderId) {
+        orderRepository.deleteOrderById(orderId);
+
+        // Removing the order from the user (mimicking referencing).
+        Order order = orderRepository.getOrderById(orderId);
+        userRepository.removeOrderFromUser(order.getUserId(), orderId);
+    }
+}
