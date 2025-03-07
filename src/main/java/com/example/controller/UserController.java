0ede77b7520d26e7929c2b11dc03d541870206c1
MohammmedAhmed8 <94786411+MohammmedAhmed8@users.noreply.github.com>
2025-03-07 13:45:13 +0200

    added user controller and user service

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,86 @@
 package com.example.controller;
 
+import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+@Tag(name = "User Controller", description = "Endpoints related to users")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/{userId}/removeOrder")
+    @Operation(
+            summary = "Remove order from user",
+            description = "Removes an order from a user by his id and the "
+                    + "order id"
+    )
+    public String removeOrderFromUser(@PathVariable final UUID userId,
+                                      @RequestParam final UUID orderId) {
+        userService.removeOrderFromUser(userId, orderId);
+        return "Order has been removed from user";
+    }
+
+    @DeleteMapping("/{userId}/emptyCart")
+    @Operation(
+            summary = "Empty cart",
+            description = "Empties the cart of a user by his id"
+    )
+    public String emptyCart(@PathVariable final UUID userId) {
+        userService.emptyCart(userId);
+        return "Cart has been emptied";
+    }
+
+    @PutMapping("/addProductToCart")
+    @Operation(
+            summary = "Add product to cart",
+            description = "Adds a product to the cart of a user by his id and "
+                    + "the product id"
+    )
+    public String addProductToCart(@RequestParam final UUID userId,
+                                   @RequestParam final UUID productId) {
+        userService.addProductToCart(userId, productId);
+        return "Product has been added to cart";
+    }
+
+    @PutMapping("/deleteProductFromCart")
+    @Operation(
+            summary = "Delete product from cart",
+            description = "Deletes a product from the cart of a user by his id "
+                    + "and the product id"
+    )
+    public String deleteProductFromCart(@RequestParam final UUID userId,
+                                        @RequestParam final UUID productId) {
+        userService.deleteProductFromCart(userId, productId);
+        return "Product has been deleted from cart";
+    }
+
+
+    @DeleteMapping("/delete/{userId}")
+    @Operation(
+            summary = "Delete user",
+            description = "Deletes a user by his id"
+    )
+    public String deleteUserById(@PathVariable final UUID userId) {
+        userService.deleteUserById(userId);
+        return "User deleted";
+    }
 
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,4 +1,74 @@
 package com.example.service;
 
-public class UserService {
+import com.example.model.Cart;
+import com.example.model.Order;
+import com.example.model.Product;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("checkstyle:GenericWhitespace")
+public class UserService extends MainService<User> {
+    private final UserRepository userRepository;
+    private final CartService cartService;
+    private final ProductService productService;
+
+    @Autowired
+    public UserService(final UserRepository userRepository,
+                       final CartService cartService,
+                       final ProductService productService) {
+        this.userRepository = userRepository;
+        this.cartService = cartService;
+        this.productService = productService;
+    }
+
+    public User addUser(final User user) {
+        return userRepository.addUser(user);
+    }
+
+    public ArrayList<User> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public User getUserById(final UUID userId) {
+        return userRepository.getUserById(userId);
+    }
+
+    public List<Order> getOrdersByUserId(final UUID userId) {
+        return userRepository.getOrdersByUserId(userId);
+    }
+
+
+    public void removeOrderFromUser(final UUID userId, final UUID orderId) {
+        userRepository.removeOrderFromUser(userId, orderId);
+    }
+
+    public void deleteUserById(final UUID userId) {
+        userRepository.deleteUserById(userId);
+    }
+
+    public void emptyCart(final UUID userId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        Cart newCart = new Cart(userId);
+        cartService.deleteCartById(cart.getId());
+        cartService.addCart(newCart);
+    }
+
+    public void addProductToCart(final UUID userId, final UUID productId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        Product product = productService.getProductById(productId);
+        cartService.addProductToCart(cart.getId(), product);
+    }
+
+    public void deleteProductFromCart(final UUID userId, final UUID productId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        Product product = productService.getProductById(productId);
+        cartService.deleteProductFromCart(cart.getId(), product);
+    }
 }
