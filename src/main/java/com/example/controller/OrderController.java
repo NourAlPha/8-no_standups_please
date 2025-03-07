5ce2564ad6ea1ed7164797cfc78b869d0e0bb698
Nour Eldien Ayman <nourayman189@gmail.com>
2025-03-07 08:54:47 +0200

    Add swagger annotations on endpoints.

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import com.example.model.Order;
 import com.example.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/order")
+@Tag(name = "Order Controller", description = "Endpoints related to orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -26,21 +29,37 @@ public class OrderController {
     }
 
     @PostMapping("/")
+    @Operation(
+            summary = "Create new order",
+            description = "Adds a new order to the system."
+    )
     public void addOrder(@RequestBody final Order order) {
         orderService.addOrder(order);
     }
 
     @GetMapping("/{orderId}")
+    @Operation(
+            summary = "Find order by ID",
+            description = "Retrieves details of specific order by Id."
+    )
     public Order getOrderById(@PathVariable final UUID orderId) {
         return orderService.getOrderById(orderId);
     }
 
     @GetMapping("/")
+    @Operation(
+            summary = "Get all orders",
+            description = "Retrieves a list of all orders in the system."
+    )
     public ArrayList<Order> getOrders() {
         return orderService.getOrders();
     }
 
     @DeleteMapping("/delete/{orderId}")
+    @Operation(
+            summary = "Remove order by ID",
+            description = "Deletes an order by its from the system."
+    )
     public String deleteOrderById(@PathVariable final UUID orderId) {
         orderService.deleteOrderById(orderId);
         return "Order deleted successfully";
