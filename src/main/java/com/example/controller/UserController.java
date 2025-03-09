973159f0d3e0951b4afe9f308e7a668da6f01e3b
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2025-03-09 22:33:26 +0200

    feat : adding first 4 endpoints and services

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,66 @@
 package com.example.controller;
 
+import com.example.model.Order;
+import com.example.model.User;
+import com.example.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "User Controller", description = "Endpoints related to Users")
+
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/")
+    @Operation(
+            summary = "Create new user",
+            description = "Adds a new user to the system."
+    )
+    public User addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/")
+    @Operation(
+            summary = "Gets all users",
+            description = "gets all the users in the system."
+    )
+    public ArrayList<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    @Operation(
+            summary = "Get specific user",
+            description = "get a specific user in the system."
+    )
+    public User getUserById(@PathVariable UUID userId){
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/{userId}/orders")
+    @Operation(
+            summary = "Get the orders of specific user",
+            description = "get the orders of specific user in the system by passing the user ID."
+    )
+    public List<Order> getOrdersByUserId(@PathVariable UUID userId){
+        return userService.getOrdersByUserId(userId);
+    }
+
+
 
 
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -1,4 +1,39 @@
 package com.example.service;
 
-public class UserService {
+import com.example.model.Order;
+import com.example.model.Product;
+import com.example.model.User;
+import com.example.repository.OrderRepository;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService extends MainService<User>{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(final User user) {
+        return userRepository.addUser(user);
+    }
+
+    public ArrayList<User> getUsers() {
+        return userRepository.getUsers();
+    }
+
+    public User getUserById(final UUID userId) {
+        return userRepository.getUserById(userId);
+    }
+
+    public List<Order> getOrdersByUserId(final UUID userId) { return userRepository.getOrdersByUserId(userId); }
+
+
 }
