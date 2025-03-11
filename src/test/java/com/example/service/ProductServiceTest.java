f80415c9ce79ee966898b97f3f140dd9308a5e59
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2025-03-11 15:40:30 +0200

    remove checkstyle plugin

--- src/test/java/com/example/service/ProductServiceTest.java
+++ src/test/java/com/example/service/ProductServiceTest.java
@@ -60,7 +60,7 @@ public class ProductServiceTest {
         assertThrows(ValidationException.class, () -> {
             productService.deleteProductById(productId);
         });
-        
+
     }
 
     @Test
